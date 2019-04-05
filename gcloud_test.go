b919091f89667e199544262f143c06d4603071f2
autom8ter <admin@autom8ter.com>
2019-04-04 20:08:50 -0600

    test

--- gcloud_test.go
+++ gcloud_test.go
@@ -2,6 +2,7 @@ package gcloud_test
 
 import (
 	"context"
+	"fmt"
 	"github.com/autom8ter/gcloud"
 	"google.golang.org/api/option"
 	"testing"
@@ -16,5 +17,21 @@ func TestNew(t *testing.T) {
 	}
 	if g == nil {
 		t.Fatal("nil gcloud")
+	}
+}
+func Test(t *testing.T) {
+	g, err := gcloud.New(ctx, option.WithCredentialsFile("credentials.json"))
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if g == nil {
+		t.Fatal("nil gcloud")
+	}
+	if err := g.Execute(func(g *gcloud.GCP) error {
+
+		fmt.Println("execution")
+		return nil
+	}); err != nil {
+		t.Fatal(err.Error())
 	}
 }
