427f8ad06c80de45f886e093363844c8601047db
autom8ter <admin@autom8ter.com>
2019-04-07 00:41:11 -0600

    done

--- gcloud_test.go
+++ gcloud_test.go
@@ -3,18 +3,14 @@ package gcloud_test
 import (
 	"context"
 	"github.com/autom8ter/gcloud"
-	"github.com/autom8ter/gcloud/handlers"
 	"testing"
 )
 
 var ctx = context.Background()
 
 func Test(t *testing.T) {
-	g, err := gcloud.New(ctx, &gcloud.Config{})
+	_, err := gcloud.New(ctx, &gcloud.Config{})
 	if err != nil {
-		t.Fatal(err.Error())
-	}
-	if err := g.Execute(handlers.Noop()); err != nil {
 		t.Fatal(err.Error())
 	}
 }
